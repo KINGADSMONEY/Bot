07f15d70fc41f3d1a2d1863b132d255042cebaf0
Guilherme <snifpvp7@gmail.com>
2018-10-02 18:43:05 -0300

    a

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 const config = require('./config.json');
 
 function changing_status() {
-    let status = ["Anúncio com +anuncio"]
+    let status = [`Anúncios com +anuncio | ${bot.guilds.size}`]
     let random = status[Math.floor(Math.random() * status.length)]
     bot.user.setActivity(random)
   }
@@ -34,6 +34,21 @@ function changing_status() {
         .setFooter(`Anunciador: ${message.author.username}`, message.author.displayAvatarURL)
         .setThumbnail(bot.user.displayAvatarURL)
         message.channel.send(anuncio);
+
       }
+      if(comando === 'reiniciar') {
+        if (message.author.id !== '430093309617111063') return;
+    
+        resetBot(message.channel)
+        async function resetBot(channel) {
+            let token = bot.token;
+            await message.react(':white_check_mark:')
+                .then(message => bot.destroy())
+                .then(message => bot.destroy())
+                .then(() => bot.login(token));
+            message.channel.send('Bot reiniciado com sucesso!')
+        }
+    }
+
   })
 bot.login(config.token);
