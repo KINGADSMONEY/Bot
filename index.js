1ab78ae3c5a41c5d538d5c1651c2d48ee454dd15
Guilherme <snifpvp7@gmail.com>
2018-10-02 16:08:46 -0300

    qualquer coisa

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config.json');
+
+function changing_status() {
+    let status = ["Anúncio com +anuncio"]
+    let random = status[Math.floor(Math.random() * status.length)]
+    bot.user.setActivity(random)
+  }
+  
+  bot.on("ready", () => {
+    console.log( `${bot.user.username} Online` );
+    setInterval(changing_status, 9000);
+  })
+
+  bot.on('message', async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+    if(!message.content.startsWith(config.prefix)) return;
+
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const comando = args.shift().toLowerCase();
+
+    if(comando === `anuncio`) {
+        message.delete();
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Desculpe, você não tem permissão para isto')
+        const sayMessage = args.join(" ");
+        let anuncio = new Discord.RichEmbed()
+        .setColor("#FFFF00")
+        .addField("📢 Anúncio 📢", `${sayMessage}`)
+        .setTimestamp()
+        .setFooter(`Anunciador: ${message.author.username}`, message.author.displayAvatarURL)
+        .setThumbnail(bot.user.displayAvatarURL)
+        message.channel.send(anuncio);
+      }
+  })
+bot.login(config.token);
