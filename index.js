7ab7738d8681fe14e84e28a2e98a664b6d2ae950
Guilherme <snifpvp7@gmail.com>
2018-10-03 20:15:25 -0300

    a

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function changing_status() {
         .setTimestamp()
         .setFooter(`Anunciador: ${message.author.username}`, message.author.displayAvatarURL)
         .setThumbnail(bot.user.displayAvatarURL)
-        message.member.guild.send(anuncio);
+        message.members.send(anuncio);
       }
 
     if(comando  === 'status'){
