3ecf5af4253da21399c00328e9b7bc19e48cb759
Guilherme <snifpvp7@gmail.com>
2018-10-03 21:49:13 -0300

    a

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function changing_status() {
         .addField("📢 Anúncio 📢", `${sayMessage}`)
         .setTimestamp()
         .setFooter(`Anunciador: ${message.author.username}`, message.author.displayAvatarURL)
-        .setFooter(`Servidor: ${guild.author.username}`, message.author.displayAvatarURL)
+        .setFooter(`Servidor: ${message.guild.name}`, message.author.displayAvatarURL)
         .setThumbnail(bot.user.displayAvatarURL)
         message.guild.members.map(membro=>{ membro.send(anuncio)})
       }
