768530ba03722f40a784e0e273007e37c82f8a40
Guilherme <snifpvp7@gmail.com>
2018-10-11 15:26:01 -0300

    nome do commit

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ bot.on('ready', () => {
     bot.user.setPresence({ game: { name: 'Em manutenção.', type: 3, url: 'https://www.twitch.tv/ladonegro'} });
 })
 
-bot.on('message', async message => {
-
-    if(message.channel.id == "495662040300519475"){
-      message.react(':sucesso:495590190950383626').then(r=>{
-        message.react(':error:495662124517949440')
-      })
-      };
-})
-
 if(comando === `anunciopv`) {
     message.delete();
     if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Desculpe, você não tem permissão para isto')
