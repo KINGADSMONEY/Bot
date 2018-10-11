0a5ae1c7ae8ffe987cae8df45f7ecd6f507a847c
Guilherme <snifpvp7@gmail.com>
2018-10-11 15:41:15 -0300

    nome do commit

--- index.js
+++ index.js
@@ -2,62 +2,53 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('./config.json');
 
-bot.on('ready', () => {
-    console.log('Iniciado com sucesso')
-    bot.user.setPresence({ game: { name: 'MANUTENÇÃO', type: 3, url: 'https://www.twitch.tv/ladonegro'} });
+function changing_status() {
+  let status = [`MANUTENÇÃO`]
+  let random = status[Math.floor(Math.random() * status.length)]
+  bot.user.setActivity(random)
+}
+
+bot.on("ready", () => {
+  console.log( `${bot.user.username} Está online sem erros.` );
+  setInterval(changing_status, 9000);
 })
 
 bot.on('message', async message => {
 
-  if(message.channel.id == "495662040300519475"){
-    message.react(':sucesso:495590190950383626').then(r=>{
-      message.react(':error:495662124517949440')
-    })
-    };
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+  if(!message.content.startsWith(config.prefix)) return;
 
-    if(comando === `anuncio`) {
-        message.delete();
-        if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Desculpe, você não tem permissão para isto')
-        const sayMessage = args.join(" ");
-        let anuncio = new Discord.RichEmbed()
-        .setColor("#FFFF00")
-        .addField("📢 Anúncio 📢", `${sayMessage}`)
-        .setTimestamp()
-        .setFooter(`Anunciador: ${message.author.username}`, message.author.displayAvatarURL)
-        .setThumbnail(bot.user.displayAvatarURL)
-        message.channel.send(anuncio);
 
-      }
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const comando = args.shift().toLowerCase();
 
-      if(comando === `anunciopv`) {
-        message.delete();
-        if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Desculpe, você não tem permissão para isto')
-        const sayMessage = args.join(" ");
-        let anuncio = new Discord.RichEmbed()
-        .setColor("#FFFF00")
-        .addField("📢 Anúncio 📢", `${sayMessage}`)
-        .setTimestamp()
-        .setFooter(`Anunciador: ${message.author.username} \n | Servidor: ${message.guild.name}`)
-        .setThumbnail(bot.user.displayAvatarURL)
-        message.guild.members.map(membro=>{ membro.send(anuncio)})
-      }
+  if(comando === `anuncio`) {
+      message.delete();
+      if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Desculpe, você não tem permissão para isto')
+      const sayMessage = args.join(" ");
+      let anuncio = new Discord.RichEmbed()
+      .setColor("#FFFF00")
+      .addField("📢 Anúncio 📢", `${sayMessage}`)
+      .setTimestamp()
+      .setFooter(`Anunciador: ${message.author.username}`, message.author.displayAvatarURL)
+      .setThumbnail(bot.user.displayAvatarURL)
+      message.channel.send(anuncio);
 
-    if (message.author.bot) return;
-    if (!message.content.startsWith(config.prefix)) return;
-
-    let command = message.content.split(" ")[0];
-    command = command.slice(config.prefix.length);
-
-    let args = message.content.split(" ").slice(1);
-    //
-
-    try {
-      let commandFile = require(`./comandos/${command}.js`);
-      commandFile.run(bot, message, args);
-    } catch (err) {
-      console.error(err);
     }
 
-})
+    if(comando === `anunciopv`) {
+      message.delete();
+      if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Desculpe, você não tem permissão para isto')
+      const sayMessage = args.join(" ");
+      let anuncio = new Discord.RichEmbed()
+      .setColor("#FFFF00")
+      .addField("📢 Anúncio 📢", `${sayMessage}`)
+      .setTimestamp()
+      .setFooter(`Anunciador: ${message.author.username} \n | Servidor: ${message.guild.name}`)
+      .setThumbnail(bot.user.displayAvatarURL)
+      message.guild.members.map(membro=>{ membro.send(anuncio)})
+    }
 
+});
 bot.login(config.token);
