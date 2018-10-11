1d6580fcb6af3dfdeeca0701a89ff4fcb62f475e
Guilherme <snifpvp7@gmail.com>
2018-10-11 14:19:49 -0300

    nome do commit

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function changing_status() {
       let embed = new Discord.RichEmbed()
       .setColor("#FFFF00")
       .setTitle("📢 Ajuda 📢")
-      .setDescription("+anuncio - Com este comando você pode fazer um anuncio no chat que você digitou o comando. \n +anunciopv - Mandar mensagem no privado de todos os jogadores do servidor \n +convidar - Você consegue o link para me convidar para seu servidor")
+      .set("+anuncio - Com este comando você pode fazer um anuncio no chat que você digitou o comando. \s \n +anunciopv - Mandar mensagem no privado de todos os jogadores do servidor \n +convidar - Você consegue o link para me convidar para seu servidor")
       .setTimestamp()
       .setFooter("Antenciosamente AnúncioBOT")
       message.channel.send(embed);
