3d4f6b005a73615b54056fe1d97d712681089f96
Guilherme <snifpvp7@gmail.com>
2018-11-05 19:39:36 -0200

    qualquer coisa

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 const config = require('./config.json');
 
 function changing_status() {
-  let status = [`|Me doa 1 real pra comprar chocolate ? /doar|`,`|Quer comprar um bot? ADD Guii__#8061|`,`|Quer saber meus comandos? /help|`,`|Estou em ${bot.guilds.size} servidores.|`,`|Me convide usando /convidar|`,`|Fui feito pelo Guii__#8061|`,`|Mande uma mensagem para meu dono /mensagem|`,`|Está solitario faça perguntar para mim /perguntar|`]
+  let status = [`|Me doa 1 real pra comprar chocolate ? /doar|`,`|Quer comprar um bot? ADD Guii__#8061|`,`|Quer saber meus comandos? /help|`,`|Estou em ${bot.guilds.size} servidores.|`,`|Me convide usando /convidar|`,`|Fui feito pelo yGuii_XD#8061|`,`|Mande uma mensagem para meu dono /mensagem|`,`|Está solitario faça perguntar para mim /perguntar|`]
 
   let random = status[Math.floor(Math.random() * status.length)]
   bot.user.setActivity(random)
