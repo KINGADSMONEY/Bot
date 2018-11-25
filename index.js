d2f00708d7860648bfd419eeb75508789447186e
Guilherme <snifpvp7@gmail.com>
2018-11-25 18:44:13 -0200

    qualquer coisa

--- index.js
+++ index.js
@@ -106,6 +106,98 @@ bot.on('message', async message => {
           .setThumbnail("https://media.giphy.com/media/Lhtui7nKVVeXS/giphy.gif")
           message.channel.send(embed)
         }
+        
+        
+if(comando === "serverinfo"){
+  const moment = require('moment')
+    moment.locale('pt-br')
+    const msg = message;
+    var time = Date.now();
+    if (!message.guild.available) return;
+    moment.locale('pt-br');
+
+
+    let guild;
+
+    if (args[0]) {
+        guild = client.guilds.get(args[0]);
+    } else {
+        guild = message.guild;
+    }
+
+    let serverRegion = {
+        'amsterdam': ':flag_nl: Amsterdã',
+        'brazil': ':flag_br: Brasil',
+        'eu-central': ':flag_eu: Europa Central',
+        'eu-west': ':flag_eu: Europa Ocidental',
+        'frankfurt': ':flag_de: Frankfurt',
+        'hongkong': ':flag_hk: Hong Kong',
+        'japan': ':flag_jp: Japão',
+        'london': ':flag_gb: Londres',
+        'russia': ':flag_ru: Russia',
+        'singapore': ':flag_sg: Singapura',
+        'sydney': ':flag_au: Sydney',
+        'us-central': ':flag_us: EUA Central',
+        'us-east': ':flag_us: EUA Oriental',
+        'us-west': ':flag_us: EUA Ocidental',
+        'us-south': ':flag_us: EUA Sul',
+    }[guild.region];
+
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    
+     .setFooter(message.author.tag,message.author.avatarURL )
+        .setTimestamp(new Date())
+    .setColor("4a2aed")
+    .setThumbnail(sicon)
+    .setTitle(`${message.guild.name}`)
+    .setFooter("©RedeZonck - Todos os direitos reservados", message.author.avatarURL) 
+    .addField(`:hammer_pick: Informações sobre o discord`, "Veja as informações do servidor!")
+    .addField(":upside_down: __Guild__ __ID__:", message.guild.id , false )
+    .addField(":gem: __Dono__ :", message.guild.owner , true )
+    .addField(":slight_smile: __Dono__ __ID__:", message.guild.owner.id  , true )
+    .addField(":pencil: __Criado__:", message.guild.createdAt , true )
+    .addField(":bell: __Região__:", `${serverRegion}` , true)
+    .addField(":star: __Emojis__: ", message.guild.emojis.size , true)
+    .addField(":radio: __Canais__", message.guild.channels.size, true)
+    .addField(":busts_in_silhouette:  __Membros__:", message.guild.memberCount , true)
+    .addField(":white_circle:  __Onlines__:", `${guild.members.filter(o => o.presence.status === 'online').size}`, true)
+    .addField(":o: __Ocupados__:",`${guild.members.filter(dnd => dnd.presence.status === 'dnd').size}`, true)
+    .addField(":open_mouth: __Ausentes__:", `${guild.members.filter(i => i.presence.status === 'idle').size}`, true) 
+    .addField(":black_circle:  __Offlines__:", `${guild.members.filter(off => off.presence.status === 'offline').size} `, true)
+
+    
+      
+    return message.channel.send(serverembed);
+}
+
+if(comando === "avatar"){
+
+  let usuario = message.guild.member(message.mentions.users.first());// || message.guild.members.get(args[0]));
+  //if(!usuario) return errors.cantfindUser(message.channel);
+
+  let semnada = new Discord.RichEmbed()
+  .setDescription(`Clique [aqui](${message.author.avatarURL}) para pegar o link da foto.`)
+  .setAuthor(`??? Imagem de perfil`)//Clique [aqui](${usuario.user.avatarURL}) para pegar o link da foto.`)
+  .setColor("#bc0000")
+  .setImage(message.author.avatarURL)
+  .setTimestamp()
+.setFooter("©Anunciador - Todos os direitos reservados", message.author.avatarURL)
+
+  if(!usuario)
+  return message.channel.send(semnada)
+
+  let msg = new Discord.RichEmbed()
+  .setDescription(`Clique [aqui](${usuario.user.avatarURL}) para pegar o link da foto.`)
+  .setAuthor(`??? Imagem de perfil`)//Clique [aqui](${usuario.user.avatarURL}) para pegar o link da foto.`)
+  .setColor("#bc0000")
+  .setImage(usuario.user.avatarURL)
+  .setTimestamp()
+  .setFooter("©Anunciador - Todos os direitos reservados", bot.user.avatarURL)
+  
+  message.channel.send(msg);
+}
+
 
         if (message.content.startsWith("/mute")) { 
           let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
