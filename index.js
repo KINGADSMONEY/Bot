ef5bb45835efa79942adb99d6cb5441151826f9f
Guilherme <snifpvp7@gmail.com>
2018-11-25 17:10:04 -0200

    qualquer coisa

--- index.js
+++ index.js
@@ -148,37 +148,30 @@ bot.on('message', async message => {
           message.reply("**Usúario mutado com sucesso!**");
       }  
 
-      const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: false});
+      client.on("message", async message => {
 
-module.exports.run = async (bot, message, args) => {
 
- 
-message.channel.send(" Enviamos no seu privado");
-
-    const Aviso = args.join(" ");
-    let semnada = new Discord.RichEmbed()
-    .setColor("edac2a")
-    .setAuthor(bot.user.username, bot.user.avatarURL)
- .setFooter("©Anunciador - Todos os direitos reservados", message.author.avatarURL)
-    .setDescription("\n\n:white_small_square: /anuncio - Com este comando você pode fazer um anuncio no chat que você digitou o comando. \n\n :white_small_square:/anunciopv - Mandar mensagem no privado de todos os jogadores do servidor \n\n :white_small_square:/convidar - Você consegue o link para me convidar para seu servidor\n\n :white_small_square:/mute - Tira a permissão de falar da pessoa que for mutada, você deve ter o cargo **SILENCIADO** criado.\n\n :white_small_square:/mensagem - Você manda uma mensagem para o meu criador.\n\n:white_small_square: /perguntar - Está solitario, faça perguntas para ele o cara mais sincero.\n\n:white_small_square:/doar - Me doe 1 real para mim comprar um **CHOCOLATE**, caso me doe seu nome é sua # irá aparecer no meus **STATUS** <3\n\n")
-
-    if(!Aviso)
-    return message.author.send(semnada)
-    message.delete().catch();
-    let aviso = new Discord.RichEmbed()
-    .setColor("edac2a")
-    .setAuthor("Toxic - Pv", "https://cdn.discordapp.com/attachments/409846357982183434/429837842697682955/emoji.png", "toxic.com.br")
-    .setDescription(Aviso)
-      
-    .setTimestamp()
-    .setFooter("©Anunciador - Todos os direitos reservados", message.author.avatarURL)
-    return message.channel.send(aviso);
-  }
-  
-module.exports.help = {
-    name: "ajuda"
-  }
+        message.channel.send(" Enviamos no seu privado");
+        
+            const Aviso = args.join(" ");
+            let semnada = new Discord.RichEmbed()
+            .setColor("edac2a")
+            .setAuthor(bot.user.username, bot.user.avatarURL)
+         .setFooter("©Anunciador - Todos os direitos reservados", message.author.avatarURL)
+            .setDescription("\n\n:white_small_square: /anuncio - Com este comando você pode fazer um anuncio no chat que você digitou o comando. \n\n :white_small_square:/anunciopv - Mandar mensagem no privado de todos os jogadores do servidor \n\n :white_small_square:/convidar - Você consegue o link para me convidar para seu servidor\n\n :white_small_square:/mute - Tira a permissão de falar da pessoa que for mutada, você deve ter o cargo **SILENCIADO** criado.\n\n :white_small_square:/mensagem - Você manda uma mensagem para o meu criador.\n\n:white_small_square: /perguntar - Está solitario, faça perguntas para ele o cara mais sincero.\n\n:white_small_square:/doar - Me doe 1 real para mim comprar um **CHOCOLATE**, caso me doe seu nome é sua # irá aparecer no meus **STATUS** <3\n\n")
+        
+            if(!Aviso)
+            return message.author.send(semnada)
+            message.delete().catch();
+            let aviso = new Discord.RichEmbed()
+            .setColor("edac2a")
+            .setAuthor("Toxic - Pv", "https://cdn.discordapp.com/attachments/409846357982183434/429837842697682955/emoji.png", "toxic.com.br")
+            .setDescription(Aviso)
+              
+            .setTimestamp()
+            .setFooter("©Anunciador - Todos os direitos reservados", message.author.avatarURL)
+            return message.channel.send(aviso);
+          });
 
 
 });
