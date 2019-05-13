06386f256fc57943ff168caf3c4bd604f777fb1c
Guilherme <snifpvp7@gmail.com>
2019-05-12 21:43:55 -0300

    qualquer coisa

--- commands/memes.js
+++ commands/memes.js
@@ -10,7 +10,7 @@ const embed = new Discord.RichEmbed()
 .setAuthor("Memes Disponiveis =D",)
 .setColor("#a32aff")
 .setThumbnail("")
-.setDescription(`**s!irineu\ns!porra\ns!ebixa\ns!tonemai\ns!avaememo\ns!fodase\ns!naosabia\ns!olokinhomeu\ns!marreta\ns!espertinho\n**`)
+.setDescription(`**s!naosei\n**b!irineu\nb!porra\nb!ebixa\nb!tonemai\nb!avaememo\nb!fodase\nb!naosabia\nb!olokinhomeu\nb!marreta\nb!espertinho\n**`)
 
 message.delete().catch(O_o=>{});  
 message.channel.send(embed);
